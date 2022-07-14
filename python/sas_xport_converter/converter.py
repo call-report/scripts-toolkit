f10690035f9232df4525b3482c51086c6d963058
Michael Handelman <michael.handelman@gmail.com>
2022-07-14 12:07:44 -0400

    adding the sas export converter

--- python/sas_xport_converter/converter.py
+++ python/sas_xport_converter/converter.py
@@ -1,104 +1,151 @@
-import pyreadstat
-from zipfile import ZipFile
-import io
+"""
+converter.py: Converts SAS XPORT format bank regulatory data files from
+the Federal Reserve Bank of Chicago
 
-from uuid import uuid4
-from tqdm import tqdm
+See README.md for requirements, usage, and additional info.
+"""
+
+import os
+import argparse
+import io
+from glob import glob
+import time
+
+from zipfile import ZipFile
 import numpy as np
 import json
 import pandas as pd
+import pyreadstat # see README.md for special instructions on pyreadstat
+from uuid import uuid4
+from tqdm import tqdm
 
-import zlib
-import os
-import argparse
-from glob import glob
 
-def bool_detector(series):
-    series_set = series.drop_duplicates()
-    if len(series_set) > 3:
+def bool_detector(series: pd.Series) -> bool:
+    """Detects if a data series contained within a pandas dataseries is boolean
+       This is needed because booleans are coded into the original data as 1 and 0,
+       instead of true/false.
+       
+       This detection should be accurate, unless a data series contains all zeros,
+       or all zeros and one "1".
+
+    Args:
+        series (pd.Series): data series to detect
+
+    Returns:
+        bool: True if if the data series is boolean, False otherwise
+    """
+    
+    # filter not NaN and drop duplicate vcalues
+    series_set = series.loc[series.apply(lambda x: not pd.isnull(x)).tolist()].drop_duplicates()
+    
+    if len(series_set) > 2:
+        # if we have more than two values, then we can't confidently determine if the data series is boolean,
+        # since the only possible values are 0 and 1
         return False
     else:
-        series_filter = sorted(list(filter(lambda x: not pd.isnull(x), list(series_set.tolist()))))
-        if series_filter == [0.0, 1.0]:
+        # check for the edge case where a data series contains only two unique integers
+        series_list = sorted(series_set.tolist())
+        if series_list == [0.0, 1.0]:
             return True
         else:
             return False
 
 
-def int_detector(series):
-    series_sum = series.sum()
+def int_detector(series: pd.Series) -> bool:
+    """Detects if a data series contained within a pandas dataseries is integer.
+    This is needed because we cannot accurately differentiate between integer and float
+    from the original data.
+    
+    Args:
+        series (pd.Series): Data series to detect
+
+    Returns:
+        bool: True if the data series is integer, False otherwise
+    """
+    # sum the series together
+    de_duped_series = series.drop_duplicates()
+    series_sum = de_duped_series.loc[de_duped_series.apply(lambda x: not pd.isnull(x)).tolist()].sum()
+    # if the modulo of the sum is 0, then it likely that the data series comprises integer
     if series_sum % 1 == 0:
         return True
     else:
+        # otherwise, it is likely that the data series does not comprise an integer
         return False
 
 
-def type_detector(series, c):
-    is_bool = False
-    is_int = False
-    is_float = False
-    is_str = False
-
-    if series.dtype == np.dtype('O'):
-        is_str = True
-    else:
-        
-        uniq_series = series.drop_duplicates()
-        # some data have very small floats..
-        uniq_series_rounded = series.apply(lambda x: round(x,7) if not pd.isnull(x) else x)
-
-        if len(uniq_series_rounded) < 3:
-            bool_filter = sorted(list(filter(lambda x: not pd.isnull(x), list(uniq_series_rounded.tolist()))))
-            if bool_filter == [0.0, 1.0]:
-                is_bool = True
-        else:
-            if uniq_series_rounded.sum() % 1 == 0:
-                is_int = True
-            else:
-                is_float = True
-
-    ret = is_bool + is_int + is_float + is_str
-    if ret == 0:
-        print("none found :", c)
-    if ret > 1:
-        print("multiple found :", c)
-    else:
-        if is_bool:
-            return "bool"
-        elif is_int:
-            return "int"
-        elif is_float:
-            return "float"
-        elif is_str:
-            return 'str'
+def type_detector(series: pd.Series, c: str) -> str:
     
+    """The field types presented by the pyreadstat library can be ambiguous.
+    This function attempts to determine the type of the data series using a series
+    of tests.
 
-def file_to_df(cur_file):
+    Returns:
+        str: field type (bool, int, float, or str)
+    """
+    
+    # if the data series type is a numpy O[bject], we can assume the data type is a string.
+    if series.dtype == np.dtype('O'):
+        return 'str'
+    elif bool_detector(series):
+        return 'bool'
+    elif int_detector(series):
+        return 'int'
+    else:
+        return 'float'
+
+def sas_xport_file_to_df(filepath: str) -> pd.DataFrame:
+    """Converts a SAS XPORT file to a pandas dataframe
+
+    Args:
+        blob (bytes): bytes blob of the SAS XPORT file
+
+    Returns:
+        pd.DataFrame: pandas dataframe representation of the SAS XPORT file
+    """
+    
+    
     df_list = []
     try:
-        generator = pyreadstat.read_file_in_chunks(pyreadstat.read_xport, cur_file, encoding="WINDOWS-1252", chunksize=4000)
-        for df, meta in generator:
+        # we read the file using the "chunks" method. /most/ files were originally encoded in WINDOWS-1252, but our final output format is UTF-8
+        generator = pyreadstat.read_file_in_chunks(pyreadstat.read_xport, filepath, encoding="WINDOWS-1252", chunksize=4000)
+        for df, _ in generator:
             df_list.append(df)
         return pd.concat(df_list)
 
     except Exception as e:
-        print("ERROR WITH", cur_file, " on ", e)
+        print("retrying read using latin-1", filepath, " on ", e)
         try:
-            generator = pyreadstat.read_file_in_chunks(pyreadstat.read_xport, cur_file, encoding="LATIN1", chunksize=4000)
-            for df, meta in generator:
+            # in some cases, the SAS XPORT file was encoded in LATIN1, so that is our fallback if the original SAS XPORT file fails to decode with WINDOWS-1252
+            generator = pyreadstat.read_file_in_chunks(pyreadstat.read_xport, filepath, encoding="LATIN1", chunksize=4000)
+            for df, _ in generator:
                 df_list.append(df)
             return pd.concat(df_list)
 
         except Exception as ee:
-            print("ANOTHER ERROR WITH", cur_file, " on ", ee)
+            # if the original SAS XPORT file fails to decode with LATIN1, then we can't decode the file,
+            # or the file is corrupt, or there is some other unspecified error
+            raise("Unable to process SAS XPORT file. Is the file corrupt? Error: ", ee)
 
-
-    pass
 
 def assemble_output(df_n: pd.DataFrame, type_dict: dict, quarter: str) -> list:
     
+    """With the normalized dataframe, type dictionary, and quarter in which the 
+    data is reported, this function assembles the output python dictionary
+    that is a representation of the original SAS data
+
+    Args:
+        df_n (pd.DataFrame): normalized dataframe
+        type_dict (dict): dictionary of field types
+        quarter: quarter in which the data is reported
+
+    Returns:
+        list: python list of dictionaries
+    """
+    
+    # create a list of dictionaries, one for each row in the dataframe
     output_dict_list = []
     
+    print("Processing data frame rows....")
     for rssd, data in tqdm(df_n.iterrows(), total=len(df_n)):
         for mdrm, value in data.items():
             new_dict_val = {}
@@ -128,12 +175,24 @@ def assemble_output(df_n: pd.DataFrame, type_dict: dict, quarter: str) -> list:
                     new_dict_val.update({"str_data": str(value), 'data_type': 'str'})
                     output_dict_list.append(new_dict_val)
             else:
+                # this should never happen
+                # TODO: raise an error here instead?
                 print("UNKNOWN TYPE:", mdrm, value)
             
     return output_dict_list
 
 
-def normalize_df(df):
+def normalize_df(df: pd.DataFrame) -> pd.DataFrame:
+    
+    """Normalizes the dataframe by aligning column names with the "time series" format used by
+       other libraries and code snippets in this repo.
+
+    Args:
+        df (pd.DataFrame): dataframe to normalize
+
+    Returns:
+        pd.DataFrame: normalized pandas dataframe
+    """
     
     normalized_df = df.copy()
     
@@ -153,7 +212,17 @@ def normalize_df(df):
 
     
 
-def process(filename):
+def process(filename: str) -> str:
+    """Takes the name of a zip file or a SAS XPORT file, processes the file,
+    and returns a list of dictionaries in JSON format, returned as a string.
+    
+
+    Args:
+        filename (str): The path of the file to process.
+
+    Returns:
+        str: JSON string representation of the list of dictionaries
+    """
     
     #  what is the file extension of filename?
     file_ext = filename.split(".")[-1]
@@ -162,6 +231,7 @@ def process(filename):
     # create empty var for scope
     data = None
     
+    # do we have a zip file that contains an XPORT file?
     if file_ext_lower == "zip":    
         # read the filename into zip_io
         zip_io = io.BytesIO(open(filename,'rb').read())
@@ -178,6 +248,8 @@ def process(filename):
             print("Found", len(xpt_file_list), "xpt files in zip file.")
             print("Processing file:", xpt_file_list[0])
             data = zip_stream.open(xpt_file_list[0]).read()
+    
+    # ...or do we have a SAS XPORT file?
     elif file_ext_lower == "xpt":
         data = open(filename, 'rb').read()
         
@@ -185,11 +257,17 @@ def process(filename):
     temp_file_name = f"/tmp/{str(uuid4())}"
     open(temp_file_name,'wb').write(data)
     
-
-    df  = file_to_df(temp_file_name)
+    # read the temp file into a pandas dataframe
+    df  = sas_xport_file_to_df(temp_file_name)
+    
+    # for the SAS XPORT files provided by FRB Chicago, the quarter date
+    # reported is going to be the same value for each row, so we can take
+    # the first value in the date column and use that as the quarter
+    
     dt_data = int(df.iloc[0].DATE)
     # normalize the df
     df_n = normalize_df(df)
+    print("Detecting column types...")
     type_dict = {c.lower(): type_detector(df[c], c) for c in tqdm(df.columns)}
     output_dict = assemble_output(df_n, type_dict, dt_data)    
 
@@ -206,13 +284,19 @@ def init_argparse() -> argparse.ArgumentParser:
     Initializes CLI argument parsing
     """
 
+    # create the CLI parser
     parser = argparse.ArgumentParser(
         usage="%(prog)s",
         description="Processes SAS XPORT files to JSON from the Federal Reserve Bank of Chicago's Commerical Bank Data Archive",
     )
+    
     parser.add_argument(
-        "-v", "--version", action="version", version= f"{parser.prog} version 0.0.2"
+        "-v", action="version", version= f"{parser.prog} version 0.0.1"
     )
+    
+    # optional output to a file instead of stdout
+    parser.add_argument('-o',  help="The file name to use for output. Otherwise, outputs to output.json.", dest="output_file_name")
+    
     parser.add_argument('files', nargs='*')
     return parser
 
@@ -222,15 +306,33 @@ def main() -> None:
     parser = init_argparse()
     args = parser.parse_args()
 
-    if len(args.files) == 0:
+
+    # how many files are we processing?
+    file_proc_length = len(args.files)
+
+    if file_proc_length == 0:
         print("\nXBRL to JSON converter\n")
         print("No files specified. Please specify at least one zip or XPT file.\n")
         return
 
+    
+    if file_proc_length > 1:
+        print("At this time, only processing one file at a time is supported.\n")
 
-    for file in args.files:
-        print("Processing file: ", file)
-        return process(file)
+    file_to_process = args.files[0]
+    
+    unix_epoch_time = str(int(time.time()))
+    possible_output_file_name = file_to_process.split(".")[0] + "_" + unix_epoch_time + ".json"
+  
+    output_str = process(file_to_process)
+    
+    if args.output_file_name: 
+        print("Writing output to file:", args.output_file_name, " note: this step may a minute or two to complete...")
+        open(args.output_file_name, 'w').write(output_str)
+    else:
+        print(f"Writing output to file, {possible_output_file_name}, note: this step may a minute or two to complete...")
+        open(possible_output_file_name, 'w').write(output_str)
+
         
 if __name__ == "__main__":
     main()
